6a4c3e0d53bc168cf98e7fcef3b7e7081d5e99e8
Pav <pav920@gmail.com>
2015-04-12 03:51:12 -0400

    sudo fixed shit.

--- jslibrary/dynamicResize.js
+++ jslibrary/dynamicResize.js
@@ -10,8 +10,10 @@ function dynamicResize(canvas, context, backCanvas, backContext, image, type, pi
 	self.currentWidth = self.canvas.width;
 	self.type = type;
     self.cachePictures = {};
-
+    self.orgImgWidth = img.width;
     self.initialCanvasWidth = $(canvas).width();
+    self.initialWindowWidth = $(window).width();
+
 
 
     self.getPixelData = function(data, w, h) {
@@ -21,60 +23,6 @@ function dynamicResize(canvas, context, backCanvas, backContext, image, type, pi
     }
 
 	self.pixelData = self.getPixelData(img, self.width, self.height);
-
-	self.downsample = function(srcImageData, width, height) {
-        var backContext = document.createElement('canvas').getContext('2d');
-        var skip = 2;
-        var result = backContext.createImageData(width/2, height/2);
-        var d = 0;
-        for (var y = 0; y < height; y += skip) {
-            for (var x = 0; x < width; x += skip) {
-                
-                for (var c = 0; c < 4; c++) {
-                    result.data[d++] = srcImageData.data[(y*width+x)*4+c];
-                }
-            }
-        }
-        srcImageData = result;
-        return srcImageData;
-    }
-
-    self.downsampleHorizontal = function(srcImageData, width, height) {
-        var backContext = document.createElement('canvas').getContext('2d');
-        var result = backContext.createImageData(width-1, height);
-        var d = 0;
-        for (var y = 0; y < height; y += 1) {
-            for (var x = 0; x < width-1; x += 1) {
-                for (var c = 0; c < 4; c++) {
-                    result.data[d++] = srcImageData.data[(y*width+x)*4+c];
-                }
-            }
-        }
-        srcImageData = result;
-        return result;
-    }
-
-	self.pyrDownHorizontally = function(srcImageData, level) {
-        var height = srcImageData.height;
-        var width = srcImageData.width;
-        for (var i = 0; i <= level; i++) {
-            console.info(i + ' ' + self.width + ' ' + self.height);
-            srcImageData = self.downsampleHorizontal(srcImageData, self.width, self.height);
-            self.width = self.width - 1;
-        }
-        return srcImageData;
-    }
-
-	self.resizeHorizontally = function(widthDiff) {
-
-		self.currentWidth = self.canvas.width;
-		self.pixelData = self.pyrDownHorizontally(self.pixelData, widthDiff);
-     //    context.clearRect ( 0 , 0 , self.canvas.width, self.canvas.height );
-    	// self.canvas.width = self.width;
-    	// context.putImageData(self.pixelData, 0, 0);
-        return self.pixelData
-	}
-
 
     self.downsampleHorizontalAndPopulateArray = function(srcImageData, width, height, pixelArray) {
         console.log("downsampleHorizontalAndPopulateArray");
@@ -104,29 +52,19 @@ function dynamicResize(canvas, context, backCanvas, backContext, image, type, pi
             width = width - 1;
 
             srcImageData = result;
-            if (i%10 == 0) {
+            //if (i%10 == 0) {
                 self.cachePictures[i] = result;
-            }
+            //}
         }
         
         return result;
     }
 
-    self.pyrDownHorizontallyAndPopulateArray = function(srcImageData, pixelArray) {
-        var height = srcImageData.height;
-        var width = srcImageData.width;
-        for (var i = 0; i <= level; i++) {
-            console.info(i + ' ' + self.width + ' ' + self.height);
-            srcImageData = self.downsampleHorizontalAndPopulateArray(srcImageData, self.width, self.height, pixelArray);
-            self.width = self.width - 1;
-        }
-        return srcImageData;
-    }
 
     self.resizeHorizontallyAndPopulateArray = function(pixelArray) {
 
         self.currentWidth = self.canvas.width;
-        self.pixelData = self.downsampleHorizontalAndPopulateArray(self.pixelData, 1904, self.height, pixelArray);
+        self.pixelData = self.downsampleHorizontalAndPopulateArray(self.pixelData, self.orgImgWidth, self.height, pixelArray);
      //    context.clearRect ( 0 , 0 , self.canvas.width, self.canvas.height );
         // self.canvas.width = self.width;
         // context.putImageData(self.pixelData, 0, 0);
@@ -135,9 +73,10 @@ function dynamicResize(canvas, context, backCanvas, backContext, image, type, pi
 
     self.setWidth = function(width) {
         context.clearRect ( 0 , 0 , self.canvas.width, self.canvas.height );
-        context.putImageData(self.cachePictures[Math.round((1903-$(window).width())/ 10) * 10], 0,0);
-        console.log(self.cachePictures[Math.round((1903-$(window).width())/ 10) * 10]);
-        $("#img-no-inseam").css('width', self.cachePictures[Math.round((1903-$(window).width())/ 10) * 10].width + "px;");
+        console.log('index: '+ width);
+
+        context.putImageData(self.cachePictures[width], 0,0);
+        $("#img-no-inseam").css('width', self.cachePictures[width].width + "px;");
     }
 
 	if (type == 'vertical') {
@@ -145,9 +84,10 @@ function dynamicResize(canvas, context, backCanvas, backContext, image, type, pi
 	} else if (type == 'horizontal') {
 		//var worker = new Worker('polarBearHorizontalWorker.js');
 		self.resizeHorizontallyAndPopulateArray(pixelArray);
-
+        console.log('INITIAL SET! '+(self.orgImgWidth-self.initialCanvasWidth));
+        self.setWidth(self.orgImgWidth-self.initialCanvasWidth);
 		$( window ).resize(function() {
-            self.setWidth(self.initialCanvasWidth - $(canvas).width());
+            self.setWidth(self.orgImgWidth- $(canvas).width());
             console.log("HI");
 		});
 	}
